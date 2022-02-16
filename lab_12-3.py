04f05187d52bb99eed8fe65827b57037560cae83
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-16 09:52:31 -0500

    add lab12 -3 and lab12-4

---
+++ lab_12-3.py
@@ -0,0 +1,5 @@
+# author: LM (AMDG) 2/16/22
+weekdays = ('Monday', 'Tuesday', 'Wednseday', 'Thursday', 'Friday')
+weekends =('Saturday', 'Sunday')
+days_of_the_week = weekdays + weekends
+print(days_of_the_week)
---
+++ lab_12-4.py
@@ -0,0 +1,4 @@
+# author: LM (AMDG) 2/16/22
+day_date = ['2', '3', '4', '5', '6']
+(mon, tue, wedn, thur, fri) =day_date
+print((mon, tue, wedn, thur, fri))
