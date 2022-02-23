dd44801a2802d6c7c5cf6189e7f7841d6343b0af
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-23 10:13:23 -0500

    add files

--- lab_12-3.py
+++ lab_12-3.py
@@ -1,5 +1,7 @@
 # author: LM (AMDG) 2/16/22
+#tuple of days of the week
 weekdays = ('Monday', 'Tuesday', 'Wednseday', 'Thursday', 'Friday')
 weekends =('Saturday', 'Sunday')
+#concatinating
 days_of_the_week = weekdays + weekends
 print(days_of_the_week)
--- lab_12-4.py
+++ lab_12-4.py
@@ -1,4 +1,6 @@
 # author: LM (AMDG) 2/16/22
+#lidt of numbers
 day_date = ['2', '3', '4', '5', '6']
+#tuple of days that sets 2 to mon... 3 to tues.. etc
 (mon, tue, wedn, thur, fri) =day_date
 print((mon, tue, wedn, thur, fri))
